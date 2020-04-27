406ffab6b5017faf3ae7030edbc6f2e94c1d236d
Yuxiang Chen <chenyuxiang8160@gmail.com>
2020-04-27 12:30:45 -0700

    first table

--- src/components/Overview.vue
+++ src/components/Overview.vue
@@ -1,12 +1,19 @@
 <template>
   <div class="container">
-    <div class="chart"></div>
+    <div class="chart">
+      <Tableau
+        url="https://public.tableau.com/views/280_test/Dashboard1?:display_count=y&publish=yes&:origin=viz_share_link"
+        width="100%"
+      ></Tableau>
+    </div>
     <div class="content">
-      <img src="../assets/pikachu_anime.png" class="fade-in" v-if="this.loaded"/>
+      <img src="../assets/pikachu_anime.png" class="fade-in" v-if="this.loaded" />
     </div>
   </div>
 </template>
 <script>
+import 'vue-tableau';
+
 export default {
   name: 'Overview',
   props: {
@@ -22,22 +29,22 @@ export default {
   /* background-color: hotpink; */
   width: 100%;
   height: 100%;
-  display: flex;
-  flex-direction: row;
-  align-items: center;
-  justify-content: space-around;
-  flex-wrap: wrap;
-  background: url('../assets/bg.jpg');
+  position: relative;
+  background: url("../assets/bg.jpg");
   background-size: cover;
   background-position: center;
 }
 .chart {
-  width: 50%;
-  height: 50%;
-  background-color: lightseagreen;
+  margin: auto;
+  padding-top: 2%;
+  width: 1400px;
+  height: 80%;
   /* min-width: 700px; */
 }
 .content {
+  position: absolute;
+  right: 0;
+  bottom: -10%;
   width: 30%;
   height: 50%;
   /* background-color: lightseagreen; */
@@ -54,7 +61,7 @@ export default {
   animation-name: fadeInOpacity;
   animation-iteration-count: 1;
   animation-timing-function: ease-in;
-  animation-duration: .6s;
+  animation-duration: 0.6s;
 }
 
 @keyframes fadeInOpacity {
